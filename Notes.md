864669b16a189e7cbf4ec545613b6fb26844bb39
DESKTOP-4NP9D3F\Tintin <tristankuisis1@gmail.com>
2019-05-19 18:41:09 +0200

    Added details on the camera. Added script to access camera via opencv

---
+++ Notes.md
@@ -0,0 +1,4 @@
+Camera Address: 192.168.1.88
+
+View IP: rtsp://192.168.1.88:554/1
+
---
+++ OpenCV/View.py
@@ -0,0 +1,15 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture('rtsp://192.168.1.88:554/1')
+
+while(True):
+
+    ret, frame = cap.read()
+    cv2.imshow('frame',frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
